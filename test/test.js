a79976281a7832d84e61a0a062734cbef5976fb4
Kumar Ranjan <kumar.ranjan.consultant@nielsen.com>
2017-11-09 13:48:48 -0500

    test cases

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var chai= require('chai');
+var assert=chai.assert;
+
+
+describe('array',function(){
+    it('should empty', function(){
+        var arr=[];
+        assert.equal(arr.length,0);
+        
+    }) ;
+});
